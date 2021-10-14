a7be655d61a8103549f0f57ce22c03447ac8459e
Illia Boichuk <boy2002chuck@gmail.com>
2021-10-15 02:06:42 +0300

    init commit

--- src/com/pb/boichuk/hw2/Calculator.java
+++ src/com/pb/boichuk/hw2/Calculator.java
@@ -1,20 +1,64 @@
-package com.pb.boichuk.hw2;
-
 import java.util.Scanner;
 
 public class Calculator {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int operand1, operand2;
-        String symbol;
-
-        operand1 = scan.nextInt();
-        operand2 = scan.nextInt();
-        symbol = scan.next();
-
-        System.out.println("x = " + operand1 + " symbol = " + symbol + " y = " + operand2);
-
+        int operand1 = getInt();
+        char sign = getsign();
+        int operand2 = getInt();
+        int result = calc(operand1, operand2, sign);
+        System.out.println("Результат операции: " + result);
     }
 
+    public static int getInt(){
+        System.out.println("Введите число: ");
+        int operand;
+        if(scanner.hasNextInt()){
+            operand = scanner.nextInt();
+        } else {
+            System.out.println("Вы допустили ошибку при вводе числа.");
+            scanner.next();
+            operand = getInt();
+        }
+        return operand;
+    }
 
+    public static char getsign(){
+        System.out.println("Введите операцию: ");
+        char sign;
+        if(scanner.hasNext()){
+            sign = scanner.next().charAt(0);
+        } else {
+            System.out.println("Вы допустили ошибку при вводе операции.");
+            scanner.next();
+            sign = getsign();
+        }
+        return sign;
+    }
+
+    public static int calc(int operand1, int operand2, char sign) {
+        int result;
+        switch (sign) {
+            case '+':
+                result = operand1+operand2;
+                break;
+            case '-':
+                result = operand1-operand2;
+                break;
+            case '*':
+                result = operand1*operand2;
+                break;
+            case '/':
+                if (operand2 != 0)
+                    result = operand1/operand2;
+                else
+                    System.out.println("На ноль делить некрасиво :/");
+                break;
+            default:
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = calc(operand1, operand2, getsign());
+        }
+        return result;
+    }
 }
