904816e28b6785c8c1225445fa587b5a5e7d8d9e
Illia Boichuk <boy2002chuck@gmail.com>
2021-10-25 01:46:48 +0300

    init commit

--- src/com/pb/boichuk/hw2/Interval.java
+++ src/com/pb/boichuk/hw2/Interval.java
@@ -9,13 +9,13 @@ public class Interval {
         int Var = in.nextInt();
         System.out.printf("Your number: %d \n", Var);
 
-        if (Var >=0 & Var <=14) {
+        if (Var >=0 && Var <=14) {
             System.out.printf(" Result: %d is [0-14]\n", Var);
-        } else if (Var >=15 & Var <=35) {
+        } else if (Var >=15 && Var <=35) {
             System.out.printf(" Result: %d is [15-35]\n", Var);
-        } else if (Var >=36 & Var <=50) {
+        } else if (Var >=36 && Var <=50) {
             System.out.printf(" Result: %d is [36-50]\n", Var);
-        } else if (Var >=51 & Var <=100) {
+        } else if (Var >=51 && Var <=100) {
             System.out.printf(" Result: %d is [51-100]\n", Var);
         } else
             System.out.printf(" Result: %d isn't [0-100]\n", Var);
--- src/com/pb/boichuk/hw3/Bingo.java
+++ src/com/pb/boichuk/hw3/Bingo.java
@@ -14,28 +14,31 @@ public class  Bingo {
         while (true) {
             Scanner sc = new Scanner(System.in);
             System.out.print("> ");
-
             int gessnumber = sc.nextInt();
-            if (gessnumber == 0) {
-                System.out.println("===[ ИГРА ОКОНЧЕНА ]===");
-                System.out.println("Вы досрочно завершили игру.");
-                System.out.println("Загаданое число было \"" + number + "\".");
-                System.out.println("===[ ИГРА ОКОНЧЕНА ]===");
-                break;
-            }
-            if (gessnumber > number) {
-                System.out.println("Почти угадали! Попытайся уменьшить число...");
-                attempts++;
-            } else if (gessnumber < number) {
-                System.out.println("Почти угадали! Попытайся увеличить число...");
-                attempts++;
-            } else {
-                System.out.println("===[ ИГРА ОКОНЧЕНА ]===");
-                System.out.println("Да! Это было число \"" + number + "\".");
-                System.out.println("Кол-во попыток - " + attempts + ".");
-                System.out.println("===[ ИГРА ОКОНЧЕНА ]===");
-                break;
-            }
+
+            if (gessnumber >= 0 && gessnumber <= 100) {
+                if (gessnumber == 0) {
+                    System.out.println("===[ ИГРА ОКОНЧЕНА ]===");
+                    System.out.println("Вы досрочно завершили игру.");
+                    System.out.println("Загаданое число было \"" + number + "\".");
+                    System.out.println("===[ ИГРА ОКОНЧЕНА ]===");
+                    break;
+                }
+                if (gessnumber > number) {
+                    System.out.println("Почти угадали! Попытайся уменьшить число...");
+                    attempts++;
+                } else if (gessnumber < number) {
+                    System.out.println("Почти угадали! Попытайся увеличить число...");
+                    attempts++;
+                } else {
+                    System.out.println("===[ ИГРА ОКОНЧЕНА ]===");
+                    System.out.println("Да! Это было число \"" + number + "\".");
+                    System.out.println("Кол-во попыток - " + attempts + ".");
+                    System.out.println("===[ ИГРА ОКОНЧЕНА ]===");
+                    break;
+                }
+            } else
+                System.out.println("Принимаются значения от 0 до 100...");
         }
     }
 }
