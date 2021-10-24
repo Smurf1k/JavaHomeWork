dd4dbd286b3d5a0f29cbffeb1b2b3614bf16dbda
Illia Boichuk <boy2002chuck@gmail.com>
2021-10-25 01:29:23 +0300

    init commit

--- src/com/pb/boichuk/hw3/Bingo.java
+++ src/com/pb/boichuk/hw3/Bingo.java
@@ -9,13 +9,20 @@ public class  Bingo {
 
         System.out.println("===[ НАЧАЛО ИГРЫ ]===");
         System.out.println("Было загадано число (от 1 до 100): ");
-        System.out.println("Для досрочного завершения введите - \"exit\".");
+        System.out.println("Для досрочного завершения введите - \"0\".");
 
         while (true) {
             Scanner sc = new Scanner(System.in);
             System.out.print("> ");
 
             int gessnumber = sc.nextInt();
+            if (gessnumber == 0) {
+                System.out.println("===[ ИГРА ОКОНЧЕНА ]===");
+                System.out.println("Вы досрочно завершили игру.");
+                System.out.println("Загаданое число было \"" + number + "\".");
+                System.out.println("===[ ИГРА ОКОНЧЕНА ]===");
+                break;
+            }
             if (gessnumber > number) {
                 System.out.println("Почти угадали! Попытайся уменьшить число...");
                 attempts++;
@@ -29,44 +36,6 @@ public class  Bingo {
                 System.out.println("===[ ИГРА ОКОНЧЕНА ]===");
                 break;
             }
-            /*if (sc.equals("exit")) {
-                break;
-            }*/
-            }
         }
-    }
-    /*
-public class S04_GuessColor {
-    // Циклическая конструкция - while.
-
-    public static void main(String[] args) {
-        System.out.println("Угадайте задуманный цвет с пяти попыток.");
-        System.out.println("Для выхода из программы введите - exit.");
-
-        final int MAX_ATTEMPT = 5; // Допустимое количество попыток.
-        int attempt = 0;           // Счетчик попыток.
-        String color = "red";      // Задуманный цвет.
-        Scanner in = new Scanner(System.in);
-
-        while (attempt < MAX_ATTEMPT) {
-            attempt++;
-            System.out.println("Попытка " + attempt + ":");
-            String value = in.next();
-
-            if (value.equals("exit")) {
-                break;
-
-            }
-
-            if (!value.equals(color)) {
-                continue;
-            }
-
-            System.out.println("Поздравляем, Вы угадали с " + attempt + " попытки!");
-            break;
-        }
-
-        System.out.println("Конец игры!");
     }
 }
-     */
