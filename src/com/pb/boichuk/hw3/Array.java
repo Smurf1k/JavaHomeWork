6a52d5c74c32dfa860b1f87741517585c7d575bf
Illia Boichuk <boy2002chuck@gmail.com>
2021-10-25 03:33:17 +0300

    init commit

--- src/com/pb/boichuk/hw3/Array.java
+++ src/com/pb/boichuk/hw3/Array.java
@@ -13,16 +13,34 @@ public class Array {
         int sumPos = 0;
 
         System.out.println("Введите " + x + " значений массива: ");
-        for(int i=0; i<len; i++){
+        for(int i = 0; i < len; i++){
             array[i] = scanner.nextInt();
         }
 
         System.out.println("Введенный массив: ");
-        for(int i=0; i<x; i++){
+        for(int i = 0; i < len; i++){
             System.out.print("[" + array[i] + "] ");
         }
 
-        for(int i=0; i<x; i++){
+        boolean Sorted = false;
+        while (!Sorted) {
+            Sorted = true;
+            for (int i = 1; i < len; i++) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    Sorted = false;
+                }
+            }
+        }
+
+        System.out.println("\nОтсортированный массив: ");
+        for(int i = 0; i < len; i++){
+            System.out.print("[" + array[i] + "] ");
+        }
+
+        for(int i=0; i < x; i++){
             sum = sum + array[i];
         }
         System.out.println("\nСумарное значение массива: " + sum);
