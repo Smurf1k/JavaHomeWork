e5da546bab2af69678e0d4e7157440a8efd0e07b
Illia Boichuk <boy2002chuck@gmail.com>
2021-10-31 22:50:34 +0200

    init commit

--- src/com/pb/boichuk/hw4/Anagram.java
+++ src/com/pb/boichuk/hw4/Anagram.java
@@ -1,5 +1,41 @@
 package com.pb.boichuk.hw4;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Anagram {
+    static void isAnagram(String str1, String str2) {
+        
+        String atrS1 = str1.replaceAll("\\s ", "");
+        String atrS2 = str2.replaceAll("\\s ", "");
+        
+        boolean status = true;
+        
+        if (atrS1.length() != atrS2.length()) {
+            status = false;
+        } else {
+            char[] ArrayatrS1 = atrS1.toLowerCase().toCharArray();
+            char[] ArrayatrS2 = atrS2.toLowerCase().toCharArray();
+            Arrays.sort(ArrayatrS1);
+            Arrays.sort(ArrayatrS2);
+            status = Arrays.equals(ArrayatrS1, ArrayatrS2);
+        }
+        System.out.println("\n===[ НАЧАЛО ПРОВЕРКИ ]===\"");
+        if (status) {
+            System.out.println("\"" + atrS1 + "\"\nИ\n\"" + atrS2 + "\"\n...\n> АНАГРАМА.");
+        } else {
+            System.out.println("\"" + atrS1 + "\"\nИ\n\"" + atrS2 + "\"\n...\n> НЕ АНАГРАМА.");
+        }
+        System.out.println("===[ КОНЕЦ ПРОВЕРКИ ]===");
+    }
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите 1 строку: ");
+        String str = in.nextLine();
+        System.out.println("Введите 2 строку: ");
+        String str2 = in.nextLine();
+
+        isAnagram(str, str2);
+
+    }
 }
