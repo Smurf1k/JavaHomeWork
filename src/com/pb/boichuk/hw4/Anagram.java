ff487a2140cb2235088f7f222e2500c1387fdd2f
Illia Boichuk <boy2002chuck@gmail.com>
2021-10-31 18:27:15 +0200

    init commit

--- src/com/pb/boichuk/hw4/Anagram.java
+++ src/com/pb/boichuk/hw4/Anagram.java
@@ -1,4 +1,5 @@
 package com.pb.boichuk.hw4;
 
 public class Anagram {
+
 }
--- src/com/pb/boichuk/hw4/CapitalLetter.java
+++ src/com/pb/boichuk/hw4/CapitalLetter.java
@@ -1,4 +1,20 @@
 package com.pb.boichuk.hw4;
 
 public class CapitalLetter {
+    public static void replaceSpaceToDot() {
+        String str = "1 000 000 000";
+        //преобразовываем строку в массив
+        char[] chArray = str.toCharArray();
+        //перебираем все элементы массива
+        for (int i = 0; i < chArray.length; i++) {
+            //находим пробел
+            if (chArray[i] == ' ') {
+                //заменяем на точку
+                chArray[i] = '.';
+
+            }
+        }
+        //выводим результат
+        System.out.println(chArray);
+    }
 }
